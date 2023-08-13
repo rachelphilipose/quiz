a9a758aaf521e4926727b09ed658395daed53169
“rachelphilipose” <rachel@voltaric.com>
2023-08-13 15:19:53 -0400

    finishing build of quiz component

--- my-quiz/src/Quiz.jsx
+++ my-quiz/src/Quiz.jsx
@@ -1,12 +1,15 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import { useState } from "react";
+import { resultInitialState } from "./constants";
 
 
 const Quiz = ({ questions }) => {
     const [currentQuestion, setCurrentQuestion ] = useState(0);
     const [answerIdx, setAnswerIndex] = useState(null);
     const [answer, setAnswer] = useState(null);
+    const [result, setResult] = useState(resultInitialState);
+    const [showResult, setShowResult] = useState(false);
 
     const { question, choices, correctAnswer } = questions[currentQuestion];
 
@@ -22,10 +25,36 @@ const Quiz = ({ questions }) => {
         }
     } 
 
+    const onClickNext = () => {
+        setAnswerIndex(null);
+        setResult((prev) =>
+            answer 
+            ? {
+                ...prev,
+                score: prev.score + 5,
+                correctAnswers: prev.correctAnswers +1
+            } : {
+                ...prev,
+                wrongAnswers: prev.correctAnswers +1
+            }
+
+        );
+        if (currentQuestion !== questions.length - 1) {
+            setCurrentQuestion((prev) => prev + 1);
+        } else {
+            setCurrentQuestion(0);
+            setShowResult(true);
+        }
+    };
+
+    const onTryAgain = () => {
+        setResult(resultInitialState);
+        setShowResult(false);
+    }
+
     return ( 
     <div className='quiz-container'> 
-
-        <>
+        {!showResult ? (<>
             <span className= "active-question-no">{currentQuestion + 1}</span>
             <span className= "total-question">/{questions.length}</span>
             <h2>{question}</h2>
@@ -39,8 +68,30 @@ const Quiz = ({ questions }) => {
                         </li>
                     ))}
             </ul>
+            <div className="footer">
+                
+                <button onClick={onClickNext} disabled={answerIdx === null}>
+                    {currentQuestion == questions.length - 1 ? "Finish": "Next"}
+                </button>
+            </div>
         </>
-    </div>
+        ) : <div className="result">
+                <h3>Result</h3>
+                <p>
+                    Total Questions: <span>{questions.length}</span>
+                </p>
+                <p>
+                    Total Score: <span>{result.score}</span>
+                </p>
+                <p>
+                    Total Correct Answers: <span>{result.correctAnswers}</span>
+                </p>
+                <p>
+                    Total Wrong Answers: <span>{result.wrongAnswers}</span>
+                </p>    
+                <button onClick = {onTryAgain}>Try Again</button>                                            
+            </div>}
+            </div>
     
     );
 };
